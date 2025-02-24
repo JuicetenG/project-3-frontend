338159cf9eb38e348ec526da7a70cbb3fc0ce5c8
JuicetenG <127890628+JuicetenG@users.noreply.github.com>
2025-02-24 12:36:40 -0500

    fix addTask

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -63,6 +63,7 @@ const Dashboard = () => {
     const newTask = await projectService.createTask(projectId, formData);
     const newCurrentProject = { ...currentProject, tasks: [...currentProject.tasks, newTask]}
     setCurrentProject(newCurrentProject);
+    
 
     const updatedProjectList = projects.map((project) => (
       project._id !== currentProject._id ? project : newCurrentProject
