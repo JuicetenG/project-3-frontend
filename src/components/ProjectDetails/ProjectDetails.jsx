1732de17c3b5a61c797c76d3010618c054238445
JuicetenG <127890628+JuicetenG@users.noreply.github.com>
2025-02-24 14:22:09 -0500

    sort categories in project details view

--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -7,18 +7,38 @@ const ProjectDetails = (props) => {
     // console.log(props)
     console.log(props);
     
+    const categorizedTasks = props.currentProject.tasks.reduce((acc, task) => {
+      if(!acc[task.category]) {
+        acc[task.category] = [];
+      } 
+
+      acc[task.category].push(task);
+      return acc;
+    }, {})
     return (
        <div className="project-details-container">
         <main className="details-container">
             <h1>{props.currentProject.title}</h1>
             <p>{props.currentProject.description}</p>
-            <ul>
+            {/* <ul>
               {props.currentProject.tasks.map((task, index) => (
                 <li key={index}>
                   {task.name}: {task.description}
                 </li>
               ))}
             </ul>
+             */}
+
+            {Object.entries(categorizedTasks).map(([category, items]) => (
+              <div key={category}>
+                <h2>{category}</h2>
+                <ul>
+                  {items.map(task => (
+                    <li key={task._id}>{task.name}: {task.description}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
             <button onClick={() => props.setCurrentProject(null)}>back</button>
         </main>
        </div>
